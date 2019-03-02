003cb0c13c0683fc75338ec1e26543a344640087
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-03-02 14:23:16 -0800

    Style tweak

--- examples/demo/planet.rs
+++ examples/demo/planet.rs
@@ -15,7 +15,7 @@ impl Planet {
 
     /// Radial heightmap function
     pub fn height_at(&self, dir: &na::Unit<na::Vector3<f64>>) -> f64 {
-        let point: [f64; 3] = (dir.into_inner() * self.radius * 0.00005).into();
+        let point: [f64; 3] = (dir.into_inner() * self.radius * 5e-5).into();
         self.noise.get(point) * 4000.0
     }
 
