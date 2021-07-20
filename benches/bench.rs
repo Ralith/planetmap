ce48c8cdaff6a58d25d3a1c1968d7099d88424f0
Benjamin Saunders <ben.e.saunders@gmail.com>
2021-07-19 18:18:46 -0700

    Benchmark some collision detection operations

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,8 +1,15 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use std::sync::Arc;
 
+use criterion::{criterion_group, criterion_main, Criterion};
+use parry3d_f64::{
+    query::{PointQuery, QueryDispatcher, Ray, RayCast},
+    shape::Ball,
+};
+
+use planetmap::parry::{FlatTerrain, Planet, PlanetDispatcher};
 use planetmap::*;
 
-criterion_group!(benches, cache);
+criterion_group!(benches, cache, collision);
 criterion_main!(benches);
 
 fn cache(c: &mut Criterion) {
@@ -39,4 +46,63 @@ fn resolve_transfers(mgr: &mut CacheManager, transfers: &mut Vec<Chunk>) {
         let slot = mgr.allocate(transfer).unwrap();
         mgr.release(slot);
     }
+}
+
+fn collision(c: &mut Criterion) {
+    const PLANET_RADIUS: f64 = 6371e3;
+    let ball = Ball { radius: 1.0 };
+    let planet = Planet::new(
+        Arc::new(FlatTerrain::new(2u32.pow(12))),
+        32,
+        PLANET_RADIUS,
+        17,
+    );
+
+    c.bench_function("intersect", |b| {
+        b.iter(|| {
+            assert!(PlanetDispatcher
+                .intersection_test(
+                    &na::Isometry3::translation(PLANET_RADIUS, 0.0, 0.0),
+                    &planet,
+                    &ball,
+                )
+                .unwrap());
+        });
+    });
+
+    c.bench_function("short raycast", |b| {
+        b.iter(|| {
+            planet.cast_local_ray(
+                &Ray {
+                    origin: na::Point3::new(PLANET_RADIUS + 1.0, 0.0, 0.0),
+                    dir: na::Vector3::y(),
+                },
+                1e1,
+                true,
+            );
+        });
+    });
+
+    c.bench_function("long raycast", |b| {
+        b.iter(|| {
+            planet.cast_local_ray(
+                &Ray {
+                    origin: na::Point3::new(PLANET_RADIUS + 1.0, 0.0, 0.0),
+                    dir: na::Vector3::y(),
+                },
+                1e3,
+                true,
+            );
+        });
+    });
+
+    c.bench_function("project point", |b| {
+        b.iter(|| {
+            planet.project_point(
+                &na::Isometry3::identity(),
+                &na::Point3::new(PLANET_RADIUS + 1.0, 0.0, 0.0),
+                true,
+            )
+        });
+    });
 }
